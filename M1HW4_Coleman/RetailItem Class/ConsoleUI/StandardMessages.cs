f1215d862ec8a233e7801c480dabfd0089ccd0ec
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-22 11:45:30 -0400

    M1HW4_Coleman

---
+++ M1HW4_Coleman/RetailItem Class/ConsoleUI/StandardMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class StandardMessages
+    {
+        public static void DisplayOpeningMenu()
+        {
+            Console.WriteLine("Enter 1 or 2 to select: ");
+            Console.WriteLine("1. Run Program");
+            Console.WriteLine("2. Exit");
+        }
+        public static void DisplayMenuError()
+        {
+            Console.WriteLine("You must enter 1 or 2.");
+        }
+
+        public static void DisplayIntError()
+        {
+            Console.WriteLine("You must enter an integer.");
+        }
+
+        public static void DisplayProgramDescription()
+        {
+            Console.WriteLine("This program stores information about retail items.");
+        }
+    }
+}
---
+++ M1HW4_Coleman/RetailItem Class/StoreLibrary/RetailItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary
+{
+    public class RetailItem
+    {
+        private string _description;
+        private int _unitsOnHand;
+        private decimal _price;
+
+        public RetailItem()
+        {
+            Description = "";
+            UnitsOnHand = 0;
+            Price = 0;
+        }
+
+        public RetailItem (string description, int unitsOnHand, decimal price)
+        {
+            Description = description;
+            UnitsOnHand = unitsOnHand;
+            Price = price;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public int UnitsOnHand
+        {
+            get
+            {
+                return _unitsOnHand;
+            }
+
+            set
+            {
+                _unitsOnHand = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                _price = value;
+            }
+        }
+    }
+}
