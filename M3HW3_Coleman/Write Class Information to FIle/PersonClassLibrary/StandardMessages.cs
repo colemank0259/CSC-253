3a9ec4eed05c4031f054f3b711c67d3d84c3ba7d
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-22 11:52:45 -0400

    M3HW3_Coleman

---
+++ M3HW3_Coleman/Write Class Information to FIle/PersonClassLibrary/StandardMessages.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClassLibrary
+{
+    public class StandardMessages
+    {
+        public string PromptInfo
+        {
+            get
+            {
+                return "This program wil take user input to make a class, " +
+                    "then takes the class info and creates a file named UserInformation.";
+            }
+        }
+
+        public string PromptFirstName
+        {
+            get
+            {
+                return "Enter first name > ";
+            }
+        }
+
+        public string PromptLastName
+        {
+            get
+            {
+                return "Enter last name > ";
+            }
+        }
+
+        public string PromptAge
+        {
+            get
+            {
+                return "Enter age > ";
+            }
+        }
+
+        public string PromptFileComplete
+        {
+            get
+            {
+                return "The information has been written to the file.";
+            }
+        }
+    }
+}
---
+++ M3HW3_Coleman/Write Class Information to FIle/PersonClassLibrary/WriteFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PersonClassLibrary
+{
+    public class WriteFile
+    {
+        public static void CreateFile(Person person)
+        {
+            try
+            {
+                StreamWriter outputFile;
+
+                outputFile = File.AppendText("UserInformation.txt");
+
+                outputFile.WriteLine(person.FirstName);
+                outputFile.WriteLine(person.LastName);
+                outputFile.WriteLine(person.Age);
+
+                outputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: The information has NOT been written to the file. Try again.");
+            }
+        }
+    }
+}
