6546a10e15fd5354edb63d6216a4f16c8750af08
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-22 10:33:43 -0400

    M1HW2_Coleman

---
+++ M1HW2_Coleman/Hospital Charges/HospitalLibrary/Charges.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary
+{
+    public class Charges
+    {
+        private int _days;
+        private decimal _medication;
+        private decimal _surgical;
+        private decimal _labFees;
+        private decimal _rehab;
+        private decimal _stayCharges;
+        private decimal _miscCharges;
+        private decimal _totalCharges;
+
+        public Charges()
+        {
+            Days = 0;
+            Medication = 0;
+            Surgical = 0;
+            LabFees = 0;
+            Rehab = 0;
+            StayCharges = 0;
+            MiscCharges = 0;
+            TotalCharges = 0;
+        }
+
+        public Charges(int days, decimal medication, decimal surgical, decimal labFees, 
+            decimal rehab, decimal stayCharges, decimal miscCharges, decimal totalCharges)
+        {
+            Days = days;
+            Medication = medication;
+            Surgical = surgical;
+            LabFees = labFees;
+            Rehab = rehab;
+            StayCharges = stayCharges;
+            MiscCharges = miscCharges;
+            TotalCharges = totalCharges;
+        }
+
+        public int Days
+        {
+            get { return _days; }
+            set { _days = value; }
+        }
+
+        public decimal Medication
+        {
+            get { return _medication; }
+            set { _medication = value; }
+        }
+
+        public decimal Surgical
+        {
+            get { return _surgical; }
+            set { _surgical = value; }
+        }
+
+        public decimal LabFees
+        {
+            get { return _labFees; }
+            set { _labFees = value; }
+        }
+
+        public decimal Rehab
+        {
+            get { return _rehab; }
+            set { _rehab = value; }
+        }
+
+        public decimal StayCharges
+        {
+            get { return _stayCharges; }
+            set { _stayCharges = value; }
+        }
+
+        public decimal MiscCharges
+        {
+            get { return _miscCharges; }
+            set { _miscCharges = value; }
+        }
+
+        public decimal TotalCharges
+        {
+            get { return _totalCharges; }
+            set { _totalCharges = value; }
+        }
+    }
+}
---
+++ M1HW2_Coleman/Hospital Charges/HospitalLibrary/Work.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary
+{
+    public class Work
+    {
+        public static Charges GetCharges()
+        {
+            // Declare the daily base rate of staying at the hospital
+            const decimal DAILY_BASE = 350m;
+
+            //Local variables
+            Charges charges = new Charges();
+
+            // Get the number of days the patient stayed at the hospital
+            InfoPrompts.Days();
+            charges.Days = int.Parse(Console.ReadLine());
+
+            // Get the patient's medication costs
+            InfoPrompts.Medication();
+            charges.Medication = decimal.Parse(Console.ReadLine());
+
+            // Get the patient's surgical costs
+            InfoPrompts.Surgical();
+            charges.Surgical = decimal.Parse(Console.ReadLine());
+
+            // Get the patient's lab fees
+            InfoPrompts.LabFees();
+            charges.LabFees = decimal.Parse(Console.ReadLine());
+
+            // Get the patient's physical rehabilitation costs
+            InfoPrompts.Rehab();
+            charges.Rehab = decimal.Parse(Console.ReadLine());
+
+            // Calculate the cost of the patient's stay at the hospital
+            charges.StayCharges = CalcStayCharges(DAILY_BASE, charges.Days);
+
+            // Calculate the patient's total miscellaneous costs
+            charges.MiscCharges = CalcMiscCharges(charges.Medication, charges.Surgical,
+                charges.LabFees, charges.Rehab);
+
+            // Calculate the the patient's total medical costs
+            charges.TotalCharges = CalcTotalCharges(charges.StayCharges, charges.MiscCharges);
+
+            // Display the output
+            Console.WriteLine($"STAY CHARGES:");
+            Console.WriteLine($"Daily Base rate for Hospital Stay: ${DAILY_BASE.ToString("n2")}");
+            Console.WriteLine($"Days Stayed: {charges.Days}");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"Total Stay Charges: ${charges.StayCharges.ToString("n2")}");
+            Console.WriteLine("");
+            Console.WriteLine($"MISCELLANEOUS CHARGES:");
+            Console.WriteLine($"Medication Costs: ${charges.Medication.ToString("n2")}");
+            Console.WriteLine($"Surgical Costs: ${charges.Surgical.ToString("n2")}");
+            Console.WriteLine($"Lab Fees: ${charges.LabFees.ToString("n2")}");
+            Console.WriteLine($"Physical Rehabilitation Costs: ${charges.Rehab.ToString("n2")}");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"Total Miscellaneous Charges: ${charges.MiscCharges.ToString("n2")}");
+            Console.WriteLine("");
+            Console.WriteLine($"TOTAL HOSPITAL CHARGES: ${charges.TotalCharges.ToString("n2")}");
+            Console.WriteLine("");
+
+            return charges;
+        }
+
+        public static decimal CalcStayCharges(decimal dailyBase, int days)
+        {
+            decimal stayCharges = 0;
+
+            stayCharges = dailyBase * days;
+
+            return stayCharges;
+            
+        }
+
+        public static decimal CalcMiscCharges(decimal medication, decimal surgical, 
+            decimal labFees, decimal rehab)
+        {
+            decimal miscCharges = medication + surgical + labFees + rehab;
+
+            return miscCharges;
+        }
+
+        public static decimal CalcTotalCharges(decimal stayCharges, decimal miscCharges)
+        {
+            decimal totalCharges = stayCharges + miscCharges;
+
+            return totalCharges;
+        }
+    }
+}
